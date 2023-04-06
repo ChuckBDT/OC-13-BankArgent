0bb0fe66162b7e92bc7ba75c4bcfd3437438d87e
ChuckBDT <charlesboudrant@protonmail.com>
2023-04-06 08:45:53 +0200

    Mocked data minor fix

--- src/views/User.jsx
+++ src/views/User.jsx
@@ -11,8 +11,8 @@ const User = () => {
   const dispatch = useDispatch();
   const accountData = [
     { title: "Argent Bank Checking (x8349)", amount: "2,082.79" },
-    { title: "Argent Bank Checking (x8349)", amount: "2,082.79" },
-    { title: "Argent Bank Checking (x8349)", amount: "2,082.79" },
+    { title: "Argent Bank Savings (x6712)", amount: "10,928.42" },
+    { title: "Argent Bank Credit Card (x8349)", amount: "184.30" },
   ];
 
   useEffect(() => {
